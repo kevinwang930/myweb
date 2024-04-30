61e5aba476cf341dfb06e22eea7a80337e839bbb
marlsen <marlsenwang@hotmail.com>
2024-04-30 12:01:19 +0800

    front-vue

--- front-vue/src/App.vue
+++ front-vue/src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/video">Video</RouterLink>
       </nav>
     </div>
   </header>
@@ -56,11 +56,11 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+@media (min-width: 512px) {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    padding-right: calc(var(--section-gap) / 4);
   }
 
   .logo {
