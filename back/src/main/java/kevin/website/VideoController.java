3db11b43e93b26bb120dc9b30a199f5305f2ad43
Kevin Wang <kevinwang09@foxmail.com>
2024-05-06 18:26:45 +0800

    range update

--- back/src/main/java/kevin/website/VideoController.java
+++ back/src/main/java/kevin/website/VideoController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.*;
+import java.nio.file.Files;
 
 @Controller
 public class VideoController {
@@ -17,50 +18,48 @@ public class VideoController {
 
     @GetMapping(path = "/video")
     public void video(HttpServletRequest request, HttpServletResponse response) {
-        Range range = getRange(request);
+        String range = request.getHeader(HttpHeaders.RANGE);
         try (
                 InputStream is = new ClassPathResource("video/video_sample.mp4").getInputStream();
                 OutputStream os = response.getOutputStream();
         ) {
-            int fileSize = is.available();
-            byte[] buffer = new byte[1024];
-            response.setContentType("video/mp4");
-            response.setHeader(HttpHeaders.CONTENT_LENGTH, "" + fileSize);
+            long start = 0, end = is.available()-1;
+            long fileLength = is.available();
+        if (range != null) {
+            String[] splits = range.split("=");
+            if (splits.length > 1) {
+                String[] ranges = splits[1].split("-");
+                start = Long.parseLong(ranges[0]);
+                if (ranges.length > 1) {
+                    end = Long.parseLong(ranges[1]);
+                }
+            }
+            long contentLength = end -start +1;
+            response.setHeader(HttpHeaders.CONTENT_RANGE,
+                               "bytes " + start + "-" + end  + "/" + fileLength);
+            response.setHeader(HttpHeaders.CONTENT_LENGTH, String.valueOf(contentLength));
+        } else {
+            response.setHeader(HttpHeaders.CONTENT_RANGE, String.valueOf(fileLength));
+            response.setHeader(HttpHeaders.CONTENT_LENGTH, String.valueOf(fileLength));
+        }
+        response.setContentType("video/mp4");
             response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
             response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "Range, X-Requested-With, Content-Type, " +
                     "Accept");
-            response.setHeader(HttpHeaders.CONTENT_RANGE,
-                               "bytes=" + range.getStart() + "-" + (range.getEnd() > range.getStart() ?
-                                       range.getEnd() :
-                                       fileSize - 1) + "/" + fileSize);
-            long rangeStart = range.getStart();
-            if (rangeStart > 0) {
-                is.skip(rangeStart);
+
+            byte[] buffer = new byte[1024];
+            if (start > 0) {
+                is.skip(start);
             }
             int flag;
             while ((flag = is.read(buffer)) != -1) {
                 os.write(buffer, 0, flag);
             }
             os.flush();
+            response.setStatus(206);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private Range getRange(HttpServletRequest request) {
-        String rangeHeader = request.getHeader(HttpHeaders.RANGE);
-        long rangeStart = 0, rangeEnd = 0;
-        if (rangeHeader != null) {
-            String[] splits = rangeHeader.split("=");
-            if (splits.length > 1) {
-                String[] ranges = splits[1].split("-");
-                rangeStart = Long.parseLong(ranges[0]);
-                if (ranges.length > 1) {
-                    rangeEnd = Long.parseLong(ranges[1]);
-                }
-            }
-        }
-        return new Range(rangeStart, rangeEnd);
     }
 }
 
